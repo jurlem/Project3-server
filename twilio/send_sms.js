a3c5df07839e9ade92cf9136dd3a4467c987ff6a
Merle Jansen <merlevanjansen@gmail.com>
2019-06-05 21:49:55 +0200

    twilio & .env working

--- twilio/send_sms.js
+++ twilio/send_sms.js
@@ -1,8 +1,10 @@
+require ('dotenv').load ();
 // Twilio Credentials
+accountSid = process.env.TWILIO_ACCOUNT_SID;
+authToken = process.env.TWILIO_AUTH_TOKEN;
 
 // require the Twilio module and create a REST client
-// const client = require ('twilio') (accountSid, authToken);
-require ('dotenv').load ();
+const client = require ('twilio') (accountSid, authToken);
 
 console.log ('Vaatame, kas asjad on defineeritud üldse.');
 console.log (process.env.TWILIO_ACCOUNT_SID);
@@ -11,10 +13,6 @@ console.log (process.env.TWILIO_AUTH_TOKEN);
 // Aga palun!
 // https://www.twilio.com/blog/2016/09/how-to-send-an-sms-with-node-js-using-twilio.html
 // https://www.twilio.com/blog/2017/08/working-with-environment-variables-in-node-js.html
-const client = require ('twilio') (
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
 
 // Create and send message
 client.messages.create (
