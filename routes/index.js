d49f1f0e8fe56760df1a1d1d06f6c2f008abb4d1
Merle Jansen <merlevanjansen@gmail.com>
2019-07-03 20:44:38 +0200

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require ('express');
 const router = express.Router ();
-const Reminders = require ('../models/Reminders');
 
 /* GET home page */
 router.get ('/', (req, res, next) => {
